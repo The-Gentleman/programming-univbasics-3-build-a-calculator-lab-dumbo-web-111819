9c5a4b6e8f9a0f3bd8a2879611cf0b3024fed734
Zeshan Raja <zraja4402@gmail.com>
2019-11-02 02:15:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+ sum = num1 + num2
+ puts sum 
 end
 
 def subtraction(num1, num2)
-
+ diff = num1 - num2
+ puts diff
 end
 
 def division(num1, num2)
-
+ quotient = num1 / num2
+ puts quotient
 end
 
 def multiplication(num1, num2)
-
+ product = num1 * num2
+ puts product
 end
 
 def modulo(num1, num2)
-
+ mod = num1 % num2
+ puts mod
 end
 
 def square_root(num)
-
+ sqa = num
 end
